84b8fd5f7b861b45e478b1693bc406f0e9183b36
Zach Lasky <zacharylasky123@gmail.com>
2019-07-18 17:53:31 -0400

    styled cards

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,15 +4,15 @@ import styled from 'styled-components';
 import Button from './Button';
 
 // STYLES
-const CardContainerDiv = styled.div`margin: 20px 200px 20px 200px;`;
-const TitleDiv = styled.div`display: flex; justify-content: center; background: #F7E796;`;
+const CardContainerDiv = styled.div`margin: 20px 200px 0px 200px;`;
+const TitleDiv = styled.div`display: flex; justify-content: center; background: #6BB35D;`;
 const TitleH1 = styled.h1`color: #bae123`;
 
-const DateDiv = styled.div`display: flex; justify-content: center;`;
+const DateDiv = styled.div`display: flex; justify-content: center; background: #5A924F`;
 
 const ImgDiv = styled.div`display: flex; justify-content: center;`;
 
-const ExplanationDiv = styled.div`display: flex; flex-direction: column; align-items: center;`;
+const ExplanationDiv = styled.div`display: flex; flex-direction: column; align-items: center; background-image: linear-gradient(#6BB35D, yellow)`;
 // const ExplanationButton = styled.button`width: 200px; font-size: 20px`;
 
 export default function Card(props) {
@@ -29,7 +29,7 @@ export default function Card(props) {
         </DateDiv>
 
         <ImgDiv>
-          <img src={data.url} alt="nasa daily"></img>
+          <img src={data.url} alt="nasa daily" width="100%"></img>
         </ImgDiv>
           
         <ExplanationDiv>
