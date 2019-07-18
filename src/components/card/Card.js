7b0ce0ada13600693b5b1573d5888c94bbff4b08
Zach Lasky <zacharylasky123@gmail.com>
2019-07-18 16:30:20 -0400

    added styled components

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TitleDiv = styled.div`display: flex; justify-content: center;`;
+const DateDiv = styled.div`display: flex; justify-content: center;`;
 
 export default function Card(props) {
   const data = props.dataProps;
     return (
-      <div>
-        <div>{data.title}</div>
-        <img src={data.url} alt="nasa image of the day"></img>
-        <div>{data.explanation}</div>
-        <div>{data.date}</div>
-        <div>{data.copyright}</div>
+      <div className="card-container">
+        
+        <TitleDiv>
+          <h1>{data.title}</h1>
+        </TitleDiv>
+
+        <DateDiv>
+          <h2>{data.date}</h2>
+        </DateDiv>
+
+        <div className="img-container">
+          <img src={data.url} alt="nasa image of the day"></img>
+        </div>
+          
+        <div className="explanation-container">
+          <h3>{data.explanation}</h3>
+        </div>
+      
       </div>
-  )
-}
+    )
+  }
--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'; 
-import Card from '../card/Card'
+import Card from '../card/Card';
 
 export default function Data() {
 
   const [data, setData] = useState([]); 
   
   useEffect(() => {
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2015-03-14")
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
    .then(response => {
      console.log("DATA:", response)
      setData(response.data);
