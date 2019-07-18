d2d2ebb01ea545047dfe492841946b2ce3ae81cb
Zach Lasky <zacharylasky123@gmail.com>
2019-07-17 22:29:01 -0400

    name change

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Card(props) {
-      const data = props.data;
-  return (
-    <div>
-      <div>{data.title}</div>
-      <img src={data.url} alt="nasa image of the day"></img>
-      <div>{data.explanation}</div>
-      <div>{data.date}</div>
-      <div>{data.copyright}</div>
-    </div>
+  const data = props.dataProps;
+    return (
+      <div>
+        <div>{data.title}</div>
+        <img src={data.url} alt="nasa image of the day"></img>
+        <div>{data.explanation}</div>
+        <div>{data.date}</div>
+        <div>{data.copyright}</div>
+      </div>
   )
 }
--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios'; 
 import Card from '../card/Card'
 
-export default function Data(props) {
+export default function Data() {
 
   const [data, setData] = useState([]); 
   
   useEffect(() => {
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14")
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2015-03-14")
    .then(response => {
      console.log("DATA:", response)
      setData(response.data);
@@ -18,6 +18,6 @@ export default function Data(props) {
   }, [])
 
   return (
-     <Card data={data}></Card>
+     <Card dataProps={data}></Card>
   )
 }
